cd53b43f43c20ba51f43f42263fcc658005be31a
dominikbenk <56314830+dominikbenk@users.noreply.github.com>
2020-01-21 18:11:34 +0100

    Add files via upload
    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+from setuptools import setup
+
+setup(name='csfd_scraper',
+      version='0.1',
+      description='School project',
+      url='https://github.com/dominikbenk/CSFD_scraper',
+      author='Dominik Benk, Miroslav Duda',
+      author_email='benk.dominik@seznam.cz',
+      license='unlicense')
+
